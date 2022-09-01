64cd0eae706d75d6cc913ac169984095383ae451
Edu Rivas <erivas17@gmail.com>
2022-09-01 14:49:54 -0400

    final details

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -10,6 +10,21 @@ class CounterScreen extends StatefulWidget {
 class _CounterScreenState extends State<CounterScreen> {
   int counter = 0;
 
+  void increase() {
+    counter++;
+    setState(() {});
+  }
+
+  void decrease() {
+    counter--;
+    setState(() {});
+  }
+
+  void reset() {
+    counter = 0;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     const TextStyle fontSize30 = TextStyle(fontSize: 30);
@@ -27,14 +42,45 @@ class _CounterScreenState extends State<CounterScreen> {
           Text(counter.toString(), style: fontSize30),
         ],
       )),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          counter++;
-          setState(() {});
-        },
-      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: CustomActionWidget(
+          increaseFn: increase, decreaseFn: decrease, resetFn: reset),
+    );
+  }
+}
+
+class CustomActionWidget extends StatelessWidget {
+  final Function increaseFn;
+  final Function decreaseFn;
+  final Function resetFn;
+
+  const CustomActionWidget({
+    Key? key,
+    required this.increaseFn,
+    required this.decreaseFn,
+    required this.resetFn,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FloatingActionButton(
+          onPressed: () => increaseFn(),
+          child: const Icon(Icons.plus_one_outlined),
+        ),
+        const SizedBox(width: 20),
+        FloatingActionButton(
+          onPressed: () => resetFn(),
+          child: const Icon(Icons.clear_outlined),
+        ),
+        const SizedBox(width: 20),
+        FloatingActionButton(
+          onPressed: () => decreaseFn(),
+          child: const Icon(Icons.exposure_minus_1_outlined),
+        ),
+      ],
     );
   }
 }
